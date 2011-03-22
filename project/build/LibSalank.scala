99d14f5e0f52a717b797e00bd759b3f55fab8479
Peter Salanki <peter@salanki.st>
2011-03-22 17:58:01 -0400

    Splitted into two subproject to be able to add Akka functionality without always having it as a dependency.

--- project/build/LibSalank.scala
+++ project/build/LibSalank.scala
@@ -1,26 +1,22 @@
 import sbt._
 import de.element34.sbteclipsify._
 
-// with AkkaProject 
-class LibSalankProject(info: ProjectInfo) extends DefaultProject(info) with Eclipsify {
-  import java.io.File
+class LibSalankParentProject(info: ProjectInfo) extends ParentProject(info) {
 
-  override def managedStyle = ManagedStyle.Maven
-   val publishTo = Resolver.file("maven-local", Path.userHome / ".m2" / "repository" asFile)
   /****** Configuration  *******/
 //  override def mainClass = Some("com.proceranetworks.psm.cmtspoller.LeasePollerMain")
-  override def compileOptions = ExplainTypes :: CompileOption("-unchecked") :: super.compileOptions.toList //  :: CompileOption("-optimise")
+//  override def compileOptions = ExplainTypes :: CompileOption("-unchecked") :: super.compileOptions.toList //  :: CompileOption("-optimise")
+//  override def testOptions = super.testOptions ++  Seq(TestArgument(TestFrameworks.ScalaTest, "-oDF"))
 
   /****** Dependencies  *******/
-  // Akka
-//  val akkaCamel = akkaModule("camel")
+  object Dependencies {
   // Logging
   private val logback_version = "0.9.17"
   val slf4j = "org.slf4j" % "slf4j-api" % "1.5.8"
-  val logback_classic = "ch.qos.logback" % "logback-classic" % logback_version
-  val logback_core = "ch.qos.logback" % "logback-core" % logback_version
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % logback_version
+  val logbackCore = "ch.qos.logback" % "logback-core" % logback_version
   // I/O
-//  val dispatch_http = "net.databinder" % "dispatch-http_2.8.0.Beta1" % "0.6.6"
+  val dispatchHttp = "net.databinder" % "dispatch-http_2.8.1" % "0.7.8"
   // val akka_persistence = "se.scalablesolutions.akka" % "akka-persistence_2.8.0"  % "0.10"
   // DNS
 //	val dnsjava = "org.dnsjava" % "dnsjava" % "2.0.6"
@@ -36,8 +32,11 @@ class LibSalankProject(info: ProjectInfo) extends DefaultProject(info) with Ecli
  // val thrift = "org.apache.thrift" % "libthrift" % "r808609" from "http://cassandra-java-client.googlecode.com/svn-history/r6/trunk/lib/libthrift-r808609.jar"
   //val cassandra = "org.apache.cassandra" % "cassandra" % "0.5.1"
  // val postgresql = "postgresql" % "postgresql" % "8.4-701.jdbc4"
-
-  // Custom repositories
+    // Testing
+   val scalaTest = "org.scalatest" % "scalatest" % "1.3"
+  }
+  
+  /******** Repositories *******/
   val guiceyfruit = "GuiceyFruit" at "http://guiceyfruit.googlecode.com/svn/repo/releases"
   val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
   val jBoss = "jBoss" at "http://repository.jboss.org/nexus/content/groups/public"
@@ -46,70 +45,55 @@ class LibSalankProject(info: ProjectInfo) extends DefaultProject(info) with Ecli
 //  val lag_net = "lag.net repository" at "http://www.lag.net/repo"
    val java_net = "java.net repository" at "http://download.java.net/maven/2"
    val oosnmp = "snmp4j repository" at "https://server.oosnmp.net/dist/release"
+   val scalaToolsSnapshots = ScalaToolsSnapshots
+  
+  // -------------------------------------------------------------------------------------------------------------------
+  // Subprojects
+  // -------------------------------------------------------------------------------------------------------------------
 
-  /******** Manifest *******/
-  override def packageOptions = super.packageOptions ++ Seq(ManifestAttributes("Implementation-Version" -> projectVersion.value.toString))
+  lazy val libsalank_core       = project("libsalank-core",   "libsalank-core",     new LibSalankCoreProject(_))
+  lazy val libsalank_akka   	= project("libsalank-akka",   "libsalank-akka",     new LibSalankAkkaProject(_),    libsalank_core)
+  
+  // -------------------------------------------------------------------------------------------------------------------
+  // libsalank-core subproject
+  // -------------------------------------------------------------------------------------------------------------------
 
-  /******** Proguard *******/
-  lazy val outputJar = outputPath / (name + "-" + version + "-standalone.jar")
-
-  val proguardJar = "net.sf.proguard" % "proguard" % "4.3" % "tools->default"
-  val toolsConfig = config("tools")
-  def rootProjectDirectory = rootProject.info.projectPath
-  val proguardConfigurationPath: Path = outputPath / "proguard.pro"
-  lazy val proguard = proguardTask dependsOn(`package`, writeProguardConfiguration)
-  private lazy val writeProguardConfiguration = writeProguardConfigurationTask dependsOn `package`
-  //lazy val pack = packTask dependsOn(proguard)
-
-  private def proguardTask = task {
-    FileUtilities.clean(outputJar :: Nil, log)
-    val proguardClasspathString = Path.makeString(managedClasspath(toolsConfig).get)
-    val configFile = proguardConfigurationPath.toString
-    println(proguardClasspathString)
-    val exitValue = Process("java", List("-Xmx1024M", "-cp", proguardClasspathString, "proguard.ProGuard", "@" + configFile)) ! log
-    if(exitValue == 0) None else Some("Proguard failed with nonzero exit code (" + exitValue + ")")
+  class LibSalankCoreProject(info: ProjectInfo) extends LibSalankDefaultProject(info) {
+	val slf4j = Dependencies.slf4j
+	val logbackClassic = Dependencies.logbackClassic
+	val logbackCore = Dependencies.logbackCore
+	val dispatchHttp = Dependencies.dispatchHttp /* Separate into I/O module */
+	val snmp = Dependencies.snmp4j /* Separate into I/O module */
+	val csv = Dependencies.opencsv
+	
+    // testing
+    val scalatest = Dependencies.scalaTest
   }
+  
+  
+  // -------------------------------------------------------------------------------------------------------------------
+  // libsalank-akka subproject
+  // -------------------------------------------------------------------------------------------------------------------
 
-  private def writeProguardConfigurationTask = 
-    task {
-      /* the template for the proguard configuration file
-       * You might try to remove "-keep class *" and "-keep class *", but this might break dynamic classloading.
-       */
-      val outTemplate = """
-	|-dontskipnonpubliclibraryclasses
-	|-dontskipnonpubliclibraryclassmembers
-	|-dontoptimize
-        |-dontobfuscate
-	|-dontshrink
-	|-dontpreverify
-        |-dontnote
-        |-dontwarn
-        |-libraryjars %s
-        |%s
-        |-outjars %s
-        |-ignorewarnings
-	|-keep class *
-	|-keep class %s { *** main(...); }
-        |"""
-      
-      val defaultJar = (outputPath / defaultJarName).asFile.getAbsolutePath
-      log.debug("proguard configuration using main jar " + defaultJar)
+  class LibSalankAkkaProject(info: ProjectInfo) extends LibSalankDefaultProject(info) with AkkaProject {
+	  //  val akkaCamel = akkaModule("camel")
+  }
+  
+  // -------------------------------------------------------------------------------------------------------------------
+  // Helpers
+  // -------------------------------------------------------------------------------------------------------------------
+  
+  class LibSalankDefaultProject(info: ProjectInfo /*, val deployPath: Path */) extends DefaultProject(info) with Eclipsify  {
+    //override def disableCrossPaths = true
 
-      val externalDependencies = Set() ++ (
-        mainCompileConditional.analysis.allExternals ++ compileClasspath.get.map { _.asFile }
-      ) map { _.getAbsoluteFile } filter {  _.getName.endsWith(".jar") }
-      
-      def quote(s: Any) = '"' + s.toString + '"'
-      log.debug("proguard configuration external dependencies: \n\t" + externalDependencies.mkString("\n\t"))
-      // partition jars from the external jar dependencies of this project by whether they are located in the project directory
-      // if they are, they are specified with -injars, otherwise they are specified with -libraryjars
-      val (externalJars, libraryJars) = externalDependencies.toList.partition(jar => Path.relativize(rootProjectDirectory, jar).isDefined)
-      log.debug("proguard configuration library jars locations: " + libraryJars.mkString(", "))
-      // exclude properties files and manifests from scala-library jar
-      val inJars = (quote(defaultJar) :: externalJars.map(quote(_) + "(!META-INF/**,!*.txt)")).map("-injars " + _).mkString("\n")
-            
-      val proguardConfiguration = outTemplate.stripMargin.format(libraryJars.map(quote).mkString(File.pathSeparator), inJars, quote(outputJar.absolutePath), mainClass.get)
-      log.debug("Proguard configuration written to " + proguardConfigurationPath)
-      FileUtilities.write(proguardConfigurationPath.asFile, proguardConfiguration, log)
-    }
+    override def compileOptions = ExplainTypes :: CompileOption("-unchecked") :: super.compileOptions.toList //  :: CompileOption("-optimise")
+    override def testOptions = super.testOptions ++  Seq(TestArgument(TestFrameworks.ScalaTest, "-oDF"))
+    
+    /******** Manifest *******/
+    override def packageOptions = super.packageOptions ++ Seq(ManifestAttributes("Implementation-Version" -> projectVersion.value.toString))
+  
+    /******** Publishing *******/
+    override def managedStyle = ManagedStyle.Maven
+    val publishTo = Resolver.file("maven-local", Path.userHome / ".m2" / "repository" asFile)
+  }
 }
--- project/plugins/projectplugins.scala
+++ project/plugins/projectplugins.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class ProjectPlugins(info: ProjectInfo) extends PluginDefinition(info) {
-  lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.6.0"
-  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "0.10"
+  lazy val eclipse = "de.element34" % "sbt-eclipsify" % "0.7.0"
+  val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.0"
+  val akkaRepo = "Akka Repo" at "http://akka.io/repository"
+
 }
